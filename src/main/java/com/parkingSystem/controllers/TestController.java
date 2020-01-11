9736d81a23c97d60674dfa5a53ad6024b86e6740
narsingraj.kommeta <narsingraj.kommeta@servicenow.com>
2020-01-11 22:43:50 +0530

    initial commit

---
+++ src/main/java/com/parkingSystem/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.parkingSystem.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping(path="/test")
+	public String testMethod(){
+		return "Hello World";
+	}
+}
